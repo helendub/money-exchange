6a9aeae180d304930b3670303db6d32063b44bb8
Alena Chyrskaya <helen.dubinskaya@gmail.com>
2019-03-13 12:07:34 +0100

    refactor: remove excess operations

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ module.exports = function makeExchange(currency) {
         if (currency > 10000) return money = {error: "You are rich, my friend! We don't have so much coins for exchange"};  
         
         let half_dollars = Math.floor(currency/50);
-        let rest = currency - half_dollars*50;
-        let quarters = Math.floor(rest/25);
-        rest = rest - quarters*25;
-        let dimes = Math.floor(rest/10);
-        rest = rest - dimes*10;
-        let nickels = Math.floor(rest/5);
-        let pennies = rest - nickels*5;
+        currency -= half_dollars*50;
+        let quarters = Math.floor(currency/25);
+        currency -= quarters*25;
+        let dimes = Math.floor(currency/10);
+        currency -= dimes*10;
+        let nickels = Math.floor(currency/5);
+        let pennies = currency - nickels*5;
    
         if (half_dollars !=0 ) money.H = half_dollars;
         if (quarters !=0 ) money.Q = quarters;
